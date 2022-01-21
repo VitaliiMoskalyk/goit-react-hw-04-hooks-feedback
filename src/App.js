daee23f3c99708ec558ba0d68c55043872889a76
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-01-21 22:40:54 +0200

    fix_warnings

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import Statistic from "./Components/Statistic";
 import FeedbackOptions from "./Components/FeedbackOptions";
 import Section from "./Components/Section";
 
+const colors = [{ "good": "#006400" }, { "neutral": "#FFD700" }, { "bad": "#FF0000" }];
+
 function App() {
   const [goodFb, setGoodFb] = useState(0);
   const [neutralFb, setNeutralFb] = useState(0);
   const [badFb, setBadFb] = useState(0);
-  
-  
-  const colors = [{"good":"#006400"},{"neutral":"#FFD700"},{"bad":"#FF0000"}]
     
   const FeedbackWriter = (kind) => {
-
     switch (kind) {
       case 'good':
-        setGoodFb(goodFb+1)
+        setGoodFb((prev)=>prev+1)
         break;
       case 'neutral':
-        setNeutralFb(neutralFb+1)
+        setNeutralFb((prev)=>prev+1)
         break;
       case 'bad':
-        setBadFb(badFb+1)
+        setBadFb((prev)=>prev+1)
         break;
       default:
         break;
